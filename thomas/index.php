7888649c55f766aa440d311eaff3af30af6700d8
ThomasConH <116417545+ThomasConH@users.noreply.github.com>
2023-06-10 22:05:41 -0500

    Trabajo Thomas

---
+++ thomas/index.php
@@ -0,0 +1,21 @@
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hotel Olympo</title>
+    
+</head>
+
+<body> 
+   
+    <h3>Hotel Olympo - Administración de habitaciones</h3>
+
+    <a href="admin/tipos_habitacion/crear.php">Crear habitacion</a><br>
+    <a href="admin/tipos_habitacion/seleccionar.php">Listar las habitaciones</a><br>
+    <a href="admin/tipos_habitacion/actualizar.php">Actualizar datos de habitacion</a>
+
+</body>
+
